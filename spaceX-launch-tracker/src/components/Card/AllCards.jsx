b9de0bf55a67034a795e036c151dc32ba821700b
Tom Okafor <tmeka008@gmail.com>
2025-01-06 09:22:35 +0100

    Add buttons for pagination.

--- spaceX-launch-tracker/src/components/Card/AllCards.jsx
+++ spaceX-launch-tracker/src/components/Card/AllCards.jsx
@@ -1,12 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CardData } from "../App";
 import Card from "./Card";
 
 const AllCards = () => {
   const spaceXLauchesData = useContext(CardData);
+
+  const launchBatch = spaceXLauchesData.filter(
+    (_, index) => index >= 0 && index < 20
+  );
+
   return (
     <main className="card-block">
-      {spaceXLauchesData.map((eachLaunch) => {
+      {launchBatch.map((eachLaunch) => {
         const {
           id,
           name,
@@ -28,33 +33,13 @@ const AllCards = () => {
           />
         );
       })}
+      {spaceXLauchesData
+        .filter((_, index) => index < Math.ceil(spaceXLauchesData.length / 10))
+        .map((_, index) => {
+          return <button key={index}>{index + 1}</button>;
+        })}
     </main>
   );
 };
 
 export default AllCards;
-
-/*
-for (let i = 0; i < spaceXLauchesData.length; i++) {
-    const {
-      id,
-      name,
-      date_local,
-      rocket,
-      details,
-      launchpad,
-      links: { webcast },
-    } = spaceXLauchesData[i];
-  return (
-    <Card
-      key={id}
-      name={name}
-      date={date_local}
-      rocket={rocket}
-      details={details}
-      launchSite={launchpad}
-      webcast={webcast}
-    />
-  );
-}
-*/
