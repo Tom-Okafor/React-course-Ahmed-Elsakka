0cd278f70c5d0171fa81ddf751ec14325a2db697
Tom Okafor <tmeka008@gmail.com>
2025-01-07 20:23:01 +0100

    Update game logic.

--- matching-cards-game/src/components/AllCards.jsx
+++ matching-cards-game/src/components/AllCards.jsx
@@ -1,8 +1,37 @@
 import Card from "./Card";
 import { SHUFFLED_IMAGES } from "../constants";
 import "../styles/AllCards.css";
+import { useState } from "react";
 
 function AllCards() {
+  const [cardStates, setCardStates] = useState({
+    clickedCardIndex: [],
+    matchedCards: [],
+    image: null,
+    haveTwoCardsBeenClicked: false,
+    hasMatchBeenMade: false,
+    matchesMade: 0,
+  });
+
+  function updateCardStates(index, image) {
+    setCardStates((prevCardStates) => {
+      return {
+        ...prevCardStates,
+        clickedCardIndex: [...prevCardStates.clickedCardIndex, index],
+        haveTwoCardsBeenClicked: prevCardStates.clickedCardIndex.length
+          ? true
+          : false,
+        image: prevCardStates.image ? prevCardStates.image : image,
+        matchesMade:
+          prevCardStates.image === image
+            ? prevCardStates.matchesMade++
+            : prevCardStates.matchesMade,
+        hasMatchBeenMade: prevCardStates.image === image ? true : false,
+      };
+    });
+  }
+
+  const { clickedCardIndex, matchedCards } = cardStates;
   return (
     <section className="card-box">
       {SHUFFLED_IMAGES.map((image, index) => (
@@ -10,6 +39,14 @@ function AllCards() {
           key={index}
           imageSource={image}
           altText={`${image.slice(8, image.length - 4)} Card`}
+          newClass={
+            clickedCardIndex.includes(index) || matchedCards.includes(index)
+              ? "change-face"
+              : ""
+          }
+          clickFunction={() => {
+            updateCardStates(index, image);
+          }}
         />
       ))}
     </section>
--- matching-cards-game/src/components/Card.jsx
+++ matching-cards-game/src/components/Card.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 
-export default function Card({ imageSource, altText, newClass }) {
+export default function Card({ imageSource, altText, newClass, clickFunction }) {
   return (
-    <div className={`card ${newClass}`}>
+    <div className={`card ${newClass}`} onClick={clickFunction}>
       <img src={imageSource} alt={altText} />
       <div className="face"></div>
     </div>
@@ -13,4 +13,5 @@ Card.propTypes = {
   imageSource: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
   newClass: PropTypes.string,
+  clickFunction: PropTypes.func
 };
