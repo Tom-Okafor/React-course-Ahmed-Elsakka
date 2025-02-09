1a2d9f944be5b732b6803357ba906d22022b7fee
Tom Okafor <tmeka008@gmail.com>
2025-02-09 01:03:08 +0100

    set up homepage by adding heading text and introductory paragraph including styles from tailwindcss classes.

--- react-router-practice/src/pages/Home/Home.jsx
+++ react-router-practice/src/pages/Home/Home.jsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Homely Supplies</h1>;
+  return (
+    <main className="h-dvh bg-[linear-gradient(to_right_top,#00000099,#00000055),url(home.jpg)] bg-no-repeat bg-cover bg-center *:text-white">
+      <div>
+        <h1 className="bold font-serif text-4xl tracking-[2px]">
+          Home Supplies
+        </h1>
+        <h3>
+          Quality You Can Trust | Designs That Inspire | Unmatched Performance
+        </h3>
+
+        <p>
+          Blending innovation and craftsmanship to create the perfect home
+          equipment for your living spaces. From state-of-the-art kitchen
+          appliances to stylish and functional furniture, we offer everything
+          you need to turn your house into a dream home.
+        </p>
+
+        <p>
+          Experience the difference with our meticulously crafted products,
+          designed to enhance your everyday life. Explore our collection and
+          find the perfect pieces to complement your unique style and needs.
+        </p>
+
+        <div className="flex gap-4 *:border-2">
+          <Link to="about">About us</Link>
+          <Link to="contact">Contact Us</Link>
+          <Link to="products">Our Products</Link>
+        </div>
+      </div>
+    </main>
+  );
 }
