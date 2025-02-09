c4c1c54cc7526e8cb526b5527fb88a2afb932713
Tom Okafor <tmeka008@gmail.com>
2025-02-09 21:06:46 +0100

    style: style h1 for both mobile and larger screens using tailwind classes.

--- react-router-practice/src/pages/Home/Home.jsx
+++ react-router-practice/src/pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <main className="h-dvh bg-[linear-gradient(to_right_top,#00000055,#00000099),url(homeImage.jpeg)] bg-no-repeat bg-cover bg-center *:text-white">
-      <div>
-        <h1 className="bold font-serif text-4xl tracking-[2px]">
+    <main className="h-dvh bg-[linear-gradient(to_right_top,#00000055,#00000099),url(homeImage.jpeg)] bg-no-repeat bg-cover bg-center *:text-white flex items-center">
+      <div className="flex flex-col gap-2.5 p-2.5">
+        <h1 className="bold font-serif text-4xl md:text-6xl tracking-widest uppercase">
           Home Supplies
         </h1>
         <h3>
