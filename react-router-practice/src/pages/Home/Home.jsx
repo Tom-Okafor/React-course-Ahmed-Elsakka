4ae444d48525de77bde6c25bd7341a6239efc8c2
Tom Okafor <tmeka008@gmail.com>
2025-02-09 23:14:42 +0100

    style: style the home page texts.

--- react-router-practice/src/pages/Home/Home.jsx
+++ react-router-practice/src/pages/Home/Home.jsx
@@ -1,24 +1,26 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const paragraphStyles =
+    "text-sm text-[#ffffffaa] md:text-lg max-w-prose [word-spacing:3px] tracking-wider leading-[1.5] backdrop-blur-[2px]";
   return (
     <main className="h-dvh bg-[linear-gradient(to_right_top,#00000055,#00000099),url(homeImage.jpeg)] bg-no-repeat bg-cover bg-center *:text-white flex items-center">
-      <div className="flex flex-col gap-2.5 p-2.5">
-        <h1 className="bold font-serif text-4xl md:text-6xl tracking-widest uppercase">
+      <div className="flex flex-col gap-2.5 px-8">
+        <h1 className="font-extrabold font-serif text-4xl md:text-6xl tracking-widest uppercase text-green-400">
           Home Supplies
         </h1>
-        <h3>
+        <h3 className="tracking-widest text-xl md:text-3xl max-w-[45ch]">
           Quality You Can Trust | Designs That Inspire | Unmatched Performance
         </h3>
 
-        <p>
+        {/* <p className={paragraphStyles}>
           Blending innovation and craftsmanship to create the perfect home
           equipment for your living spaces. From state-of-the-art kitchen
           appliances to stylish and functional furniture, we offer everything
           you need to turn your house into a dream home.
-        </p>
+        </p> */}
 
-        <p>
+        <p className={paragraphStyles}>
           Experience the difference with our meticulously crafted products,
           designed to enhance your everyday life. Explore our collection and
           find the perfect pieces to complement your unique style and needs.
@@ -26,8 +28,8 @@ export default function Home() {
 
         <div className="flex gap-4 *:border-2">
           <Link to="about">About us</Link>
-          <Link to="contact">Contact Us</Link>
           <Link to="products">Our Products</Link>
+          <Link to="contact">Contact Us</Link>
         </div>
       </div>
     </main>
