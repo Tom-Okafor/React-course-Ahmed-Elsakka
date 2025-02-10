cc26eda02e8dca69681151119f91c21880c33df2
Tom Okafor <tmeka008@gmail.com>
2025-02-10 13:00:13 +0100

    style: add unique styles for the home page buttons for mobile screens.

--- react-router-practice/src/pages/Home/Home.jsx
+++ react-router-practice/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const paragraphStyles =
-    "text-sm text-[#ffffffaa] md:text-lg max-w-prose [word-spacing:3px] tracking-wider leading-[1.5] backdrop-blur-[5px] p-4 shadow-[0px_0px_8px_#ffffff] rounded-xl hover:bg-[linear-gradient(#000000aa,#000000aa),url(homeImage.jpeg)] bg-contain bg-fixed duration-500";
+    "text-sm text-[#ffffffcc] md:text-lg max-w-prose [word-spacing:3px] tracking-wider leading-[1.5] backdrop-blur-[5px] p-4 shadow-[0px_0px_8px_#ffffff] rounded-xl hover:bg-[linear-gradient(#000000aa,#000000aa)] bg-contain bg-fixed duration-500";
   return (
     <main className="h-dvh bg-[linear-gradient(to_right_top,#00000055,#00000099),url(homeImage.jpeg)] bg-no-repeat bg-cover bg-fixed bg-center *:text-white flex items-center">
       <div className="flex flex-col gap-2.5 px-8">
@@ -26,7 +26,7 @@ export default function Home() {
           find the perfect pieces to complement your unique style and needs.
         </p>
 
-        <div className="flex gap-4 items-center *:border-2 *:bg-green-800 *:hover:bg-green-600 *:duration-300 tracking-[3px] *:py-3 *:px-6 *:rounded-4xl *:backdrop-blur-[2px] mt-4 font-semibold font-serif">
+        <div className="flex gap-3 flex-wrap items-center *:border-2 *:bg-green-800 *:hover:bg-green-600 *:duration-300 tracking-[1px] md:tracking-[3px] *:py-2 *:px-4 text-sm md:text-lg md:*:py-3 md:*:px-6 *:rounded-4xl mt-4 font-semibold font-serif">
           <Link to="about">About us</Link>
           <Link to="products">Our Products</Link>
           <Link to="contact">Contact Us</Link>
