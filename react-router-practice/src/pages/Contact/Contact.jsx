91b2b62d70b99c8142c7b5133af4cd05569a99c3
Tom Okafor <tmeka008@gmail.com>
2025-02-26 23:42:59 +0100

    update files.

--- react-router-practice/src/pages/Contact/Contact.jsx
+++ react-router-practice/src/pages/Contact/Contact.jsx
@@ -1,20 +1,20 @@
 export default function Contact() {
   return (
-    <section className="flex items-center flex-col gap-8 py-12">
+    <section className="flex items-center flex-col gap-8 py-12 show">
       <h2 className="uppercase text-green-600 font-black text-3xl md:text-6xl font-serif tracking-widest py-4 px-8 about-h1">
         Contact Us
       </h2>
-      <p className="text-zinc-600 text-lg md:text-2xl font-semibold font-serif max-w-[40ch] text-center">
+      <p className="text-zinc-600 text-lg md:text-2xl font-semibold font-serif max-w-[40ch] text-center show-text">
         Reach to us and get a swift response and/or feedback from our customer
         care representatives.
       </p>
-      <form className="flex flex-col items-center *:border-2 *:rounded-lg w-[60vw] max-w-[500px] *:w-full gap-4 *:py-2 *:px-4 *:border-green-500 *:shadow-md">
+      <form className="flex flex-col items-center *:border-2 *:rounded-lg w-[60vw] max-w-[500px] *:w-full gap-4 *:py-4 *:px-4 *:border-green-500 *:shadow-md show">
         <input type="text" placeholder="Name" required />
         <input type="email" placeholder="Email" required />
         <textarea placeholder="Message" rows="5" required></textarea>
         <button
           type="submit"
-          className="bg-green-400 hover:bg-green-800 hover:text-white duration-300"
+          className="bg-green-400 hover:bg-green-800 hover:text-white duration-300 text-xl font-semibold font-mono tracking-widest text-green-950"
         >
           Submit
         </button>
